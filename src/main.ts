9ac228152309022cb9f625ad57f11af10eb5351c
tompston <tompston@protonmail.com>
2023-02-04 19:04:27 +0200

    first commit

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import './assets/css/index.css'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+
+// prompt -> AI robot grading homework from a student, intricate, elegant, highly detailed, painting, smooth, sharp focus, minimal,llustration, 8k
